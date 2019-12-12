e905a389a4fcf9612c76bba59b91d6bda8b8b21d
ariel <arielgordon123@gmail.com>
2019-12-12 13:57:14 +0200

    pwa changes

--- plugins/pwa.client.js
+++ plugins/pwa.client.js
@@ -1,6 +1,12 @@
-window.addEventListener("beforeinstallprompt", e => {
-  e.userChoice.then(choiceResult => {
-    ga("send", "event", "A2H", choiceResult.outcome);
-    app.$gtm.pushEvent({ event: "A2H", ...choiceResult.outcome })
+export default ({ app }, inject) => {
+  // Set the function directly on the context.app object
+  // console.log('app :', app.$gtm.pushEvent({ event: 'myEvent' }));
+  window.addEventListener("beforeinstallprompt", e => {
+    console.log("beforeinstallprompt");
+    e.userChoice.then(choiceResult => {
+      console.log("choiceResult :", choiceResult);
+      //   ga("send", "event", "A2H", choiceResult.outcome);
+      app.$gtm.pushEvent({ event: "A2H", ...choiceResult.outcome });
+    });
   });
-});
+};
