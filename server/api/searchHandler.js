df6925ff44aa938fb023f27d3a26cd0c0c062677
ariel <arielgordon123@gmail.com>
2019-12-12 21:55:18 +0200

    remove console log

--- server/api/searchHandler.js
+++ server/api/searchHandler.js
@@ -5,16 +5,16 @@ const router = express.Router();
 const Item = require("../models/Item");
 
 router.get("/:query", async (req, res) => {
-  console.log("in route search:", req.params.query);
+  // console.log("in route search:", req.params.query);
 
   if (!isNaN(req.params.query)) {
     // search by serial
-    console.log('search by number :');
+    // console.log('search by number :');
     const items = await Item.find({ serial: { $eq: Number(req.params.query)  }}, "_id name");
     res.json(items);
   } else {
     //  search by text
-    console.log('search by text :');
+    // console.log('search by text :');
     const items = await Item.find({$text: { $search: req.params.query} }, "_id name");
     res.json(items);
   }
